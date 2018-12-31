7255cc68c74d16e965b38958518afbc78c177c90
kindred_fp <kindredfp@users.noreply.github.com>
2018-12-30 22:56:52 -0500

    Added array method calls

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ function firstFunction (name){
 	return name;
 }
 
+function addElement (myArray,myVariable){
+	myArray.push(myVariable);
+	return myArray;
+}
+
+function removeElement (myArray){
+	myArray.pop();
+	return myArray;
+}
+
 let firstName = 'Francis';
 
 //Print the function
